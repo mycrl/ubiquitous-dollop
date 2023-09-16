ce8f998769b95e15d234b74fd6f126a4444f9aba
Mr.Panda <xivistudios@gmail.com>
2023-09-16 16:03:39 +0800

    feat: include librtc module;

--- build.rs
+++ build.rs
@@ -6,6 +6,10 @@ fn join(root: &str, next: &str) -> String {
     Path::new(root).join(next).to_str().unwrap().to_string()
 }
 
+fn is_exsit(dir: &str) -> bool {
+    fs::metadata(dir).is_ok()
+}
+
 #[cfg(target_os = "windows")]
 fn exec(cmd: &str, work_dir: &str) -> Result<ExitStatus, std::io::Error> {
     Command::new("powershell")
@@ -16,6 +20,8 @@ fn exec(cmd: &str, work_dir: &str) -> Result<ExitStatus, std::io::Error> {
 
 fn main() {
     let cef_version = "cef_binary_116.0.22+g480de66+chromium-116.0.5845.188";
+    let librtc_version = "librtc-v0.1.x";
+    
     let is_debug = env::var("DEBUG")
         .map(|label| label == "true")
         .unwrap_or(true);
@@ -27,8 +33,13 @@ fn main() {
 
     let temp = env::var("TEMP").unwrap();
     let temp_cef = join(&temp, cef_version);
+    let temp_librtc = join(&temp, librtc_version);
 
-    if fs::metadata(&join(&out_dir, "./locales")).is_err() {
+    if !is_exsit(&join(&out_dir, "./locales")) {
         exec(&format!("cp -r {}/* ./", &temp_cef), &out_dir).unwrap();
+    }
+    
+    if !is_exsit(&join(&out_dir, "./avcodec-59.dll")) {
+        exec(&format!("cp -r {}/* ./", &temp_librtc), &out_dir).unwrap();
     }
 }
